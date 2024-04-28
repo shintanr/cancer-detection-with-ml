27e3b9cbf809ace4284446dc7a4fff475ef5c785
shintanr <91821725+shintanr@users.noreply.github.com>
2024-04-28 07:50:47 +0700

    Selecting an image from the gallery and showing it

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.dicoding.asclepius.view
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 
@@ -14,29 +21,87 @@ class MainActivity : AppCompatActivity() {
 
     private var currentImageUri: Uri? = null
 
+//    private val requestPermissionLauncher = registerForActivityResult(
+//        ActivityResultContracts.RequestPermission()
+//    ) { isGranted: Boolean ->
+//        if(isGranted) {
+//            Toast.makeText(this, "permission is granted", Toast.LENGTH_LONG).show()
+//        } else {
+//            Toast.makeText(this, "permission request denied", Toast.LENGTH_LONG).show()
+//        }
+//    }
+
+//    private fun allPermissionIsGranted() =
+//        ContextCompat.checkSelfPermission(
+//            this,
+//            REQUIRED_PERMISSION
+//        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        if(!allPermissionIsGranted()){
+//            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+//        }
+
+        binding.galleryButton.setOnClickListener { startGallery()}
+        binding.analyzeButton.setOnClickListener {
+            currentImageUri?.let {
+                analyzeImage(it)
+            } ?: run {
+                showToast(getString(R.string.empty_image_warning))
+            }
+        }
+
     }
 
     private fun startGallery() {
         // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
 
     private fun showImage() {
         // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            Log.d("image URI", " showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
-    private fun analyzeImage() {
+//    private fun moveToResult() {
+//        val intent = Intent(this, ResultActivity::class.java)
+//        startActivity(intent)
+//    }
+    private fun analyzeImage(uri: Uri) {
         // TODO: Menganalisa gambar yang berhasil ditampilkan.
-    }
-
-    private fun moveToResult() {
         val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, uri.toString())
         startActivity(intent)
     }
+
+
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
+
+
 }
--- app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -1,19 +1,32 @@
 package com.dicoding.asclepius.view
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
 
-        // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+
     }
 
-
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_RESULT = "extra_result"
+    }
 }
